cce08ac02cc90e73bb50a5ab3aa6fc910dc48830
Mez29 <mez_29@comunidad.unam.mx>
2021-01-19 12:53:27 -0600

    Clase 19/01/2021

---
+++ Arboles/pruebas_abb.py
@@ -0,0 +1,12 @@
+#CLASE 19/01/2021
+
+from Arboles_binarios import BinatySearchTree
+
+abb = BinatySearchTree()
+abb.insert(50)
+abb.insert(30)
+abb.insert(60)
+abb.insert(35)
+abb.insert(89)
+
+abb.transversal()
